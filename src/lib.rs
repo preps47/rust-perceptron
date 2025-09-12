a7e7b30e1a3086a73cd54f92cdb9a94a950a15ae
preps47 <marco@premoli.net>
2025-09-12 06:43:32 +0200

    Some changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,25 @@
 pub struct Perceptron {
     values: Vec<Vec<f64>>,
+    solutions: Vec<u8>,
     weights: Vec<f64>,
     bias: f64,
     eta: f64
 }
 
 impl Perceptron {
-    pub fn new(values: Vec<Vec<f64>>, weights: Vec<f64>, bias: f64, eta: f64) -> Self {
-        Perceptron { values, weights, bias, eta }
+    pub fn new(values: Vec<Vec<f64>>, solutions: Vec<u8>, weights: Vec<f64>, bias: f64, eta: f64) -> Self {
+        Perceptron { values, solutions, weights, bias, eta }
     }
     
-    pub fn execute(&mut self, f: impl Fn(Vec<Vec<f64>>, Vec<f64>, f64, f64) -> Perceptron) {
-        let perceptron = f(
+    pub fn execute(&mut self, f: impl Fn(Vec<Vec<f64>>, Vec<u8>, Vec<f64>, f64, f64) -> (Vec<f64>, f64)) {
+        let new_params = f(
             self.values.clone(), 
+            self.solutions.clone(),
             self.weights.clone(),
             self.bias, 
             self.eta
         );
-        self.weights = perceptron.weights;
-        self.bias = perceptron.bias;
+        self.weights = new_params.0;
+        self.bias = new_params.1;
     }
 }
